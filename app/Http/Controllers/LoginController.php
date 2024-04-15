8b03f3440b04424410d82ae75e15cfbfe2784206
madhavkumar <madhavkumar504@gmail.com>
2024-04-15 16:41:06 +0530

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        $username = $request->username;
+        $password = $request->password;
+
+        $user = new User();
+        $user->username = $username;
+        $user->password = $password;
+        $response = $user->save();
+        $final = [
+            "status" => 200,
+            "message" => "saved successfully!"
+        ];
+
+        return response()->json(['status' => 200, $final]);
+
+    }
+}
