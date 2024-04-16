a271fd925591440f62626929ded7099539373d4b
madhavkumar <madhavkumar504@gmail.com>
2024-04-16 16:44:21 +0530

    demo

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //Yaaa hooo
+    //Rishav
     public function login(Request $request){
         $username = $request->username;
 
